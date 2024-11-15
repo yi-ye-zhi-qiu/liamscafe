a09d7662f9fe8221e7f3b7cca393b32435f974f2
Liam Isaacs <yeqiu194@gmail.com>
2024-11-15 13:31:08 -0500

    (fix): Comment out checkout behavior

--- components/checkOut.js
+++ components/checkOut.js
@@ -1,37 +1,41 @@
-import Link from "next/link";
-import { useCartContext } from "../context/cart";
-
-export default function CheckOut() {
-  const [cart, setCart] = useCartContext();
-  return (
-    <>
-      <div className="max-w-xl w-1/2 m-auto pb-16">
-        <button
-          className={`w-full transition-opacity rounded text-2xl text-center p-4 font-bold
-            ${
-              cart.length == 0
-                ? "bg-slate-100 opacity-30 cursor-not-allowed"
-                : ""
-            }                        
-          `}
-          title={
-            cart.length == 0
-              ? "Cannot check-out with empty cart. Please add items before checking out."
-              : ""
-          }
-          disabled={cart.length == 0 ? true : false}
-        >
-          <Link
-            href={{
-              pathname: "/receipt",
-            }}
-            as={`/receipt`}
-            className={cart.length == 0 ? "pointer-events-none" : ""}
-          >
-            Check Out
-          </Link>
-        </button>
-      </div>
-    </>
-  );
-}
+// Module that will check if a user's cart has items, and let them
+// checkout. This is in support of pages/receipt.js, which is
+// currently in progress.
+//
+// import Link from "next/link";
+// import { useCartContext } from "../context/cart";
+// 
+// export default function CheckOut() {
+//   const [cart, setCart] = useCartContext();
+//   return (
+//     <>
+//       <div className="max-w-xl w-1/2 m-auto pb-16">
+//         <button
+//           className={`w-full transition-opacity rounded text-2xl text-center p-4 font-bold
+//             ${
+//               cart.length == 0
+//                 ? "bg-slate-100 opacity-30 cursor-not-allowed"
+//                 : ""
+//             }                        
+//           `}
+//           title={
+//             cart.length == 0
+//               ? "Cannot check-out with empty cart. Please add items before checking out."
+//               : ""
+//           }
+//           disabled={cart.length == 0 ? true : false}
+//         >
+//           <Link
+//             href={{
+//               pathname: "/receipt",
+//             }}
+//             as={`/receipt`}
+//             className={cart.length == 0 ? "pointer-events-none" : ""}
+//           >
+//             Check Out
+//           </Link>
+//         </button>
+//       </div>
+//     </>
+//   );
+// }
--- pages/receipt.js
+++ pages/receipt.js
@@ -1,24 +1,28 @@
-import { useEffect } from "react";
-import ReceiptCart from "../components/receiptCart";
-import { useCartContext } from "../context/cart";
-import { useOverlayContext } from "../context/overlay";
+// TODO: Add receipt of user (checkout functionality)
+//
+// import { useEffect } from "react";
+// import ReceiptCart from "../components/receiptCart";
+// import { useCartContext } from "../context/cart";
+// import { useOverlayContext } from "../context/overlay";
+// 
+// export default function Receipt() {
+//   const [cart, setCart] = useCartContext();
+//   const [overlayIndex, setOverlayIndex] = useOverlayContext();
+// 
+//   return (    
+//     <div className="h-screen">
+//       <div
+//         className={`${overlayIndex != -1 ? "fixed" : "hidden"}
+//           z-20 bg-white sm:bg-transparent backdrop-blur-sm h-[100vh] w-full`}
+//         // If user clicks outside of whatever is on top of overlay,
+//         // closes overlay.
+//         onClick={() => {
+//           setOverlayIndex(-1);
+//         }}
+//       />
+//       <ReceiptCart cart={cart} />
+//     </div>
+//   );
+// }
 
-export default function Receipt() {
-  const [cart, setCart] = useCartContext();
-  const [overlayIndex, setOverlayIndex] = useOverlayContext();
-
-  return (    
-    <div className="h-screen">
-      <div
-        className={`${overlayIndex != -1 ? "fixed" : "hidden"}
-          z-20 bg-white sm:bg-transparent backdrop-blur-sm h-[100vh] w-full`}
-        // If user clicks outside of whatever is on top of overlay,
-        // closes overlay.
-        onClick={() => {
-          setOverlayIndex(-1);
-        }}
-      />
-      <ReceiptCart cart={cart} />
-    </div>
-  );
-}
+export default function Receipt() {<></>}
